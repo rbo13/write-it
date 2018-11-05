80236a2081b1fca2f5f5f1e3c32855b1d40ecb18
Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>
2018-11-05 21:21:01 +0800

    Added Update and Create of Posts
    Signed-off-by: Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>

--- app/usecase/post.go
+++ app/usecase/post.go
@@ -3,6 +3,10 @@ package usecase
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi"
 
 	"github.com/go-chi/render"
 	"github.com/rbo13/write-it/app"
@@ -40,17 +44,76 @@ func (p *postUsecase) Create(w http.ResponseWriter, r *http.Request) {
 }
 
 func (p *postUsecase) Get(w http.ResponseWriter, r *http.Request) {
+	posts, err := p.postService.Posts()
 
+	if err != nil {
+		render.JSON(w, r, err.Error())
+		return
+	}
+	render.JSON(w, r, posts)
 }
 
 func (p *postUsecase) GetByID(w http.ResponseWriter, r *http.Request) {
+	postID, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 64)
 
+	if err != nil {
+		render.JSON(w, r, err.Error())
+		return
+	}
+
+	post, err := p.postService.Post(postID)
+
+	if err != nil {
+		render.JSON(w, r, err.Error())
+		return
+	}
+
+	render.JSON(w, r, post)
 }
 
 func (p *postUsecase) Update(w http.ResponseWriter, r *http.Request) {
+	var post app.Post
+	postID, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 64)
 
+	if err != nil {
+		render.JSON(w, r, err)
+		return
+	}
+
+	post.ID = postID
+	post.UpdatedAt = time.Now()
+
+	err = json.NewDecoder(r.Body).Decode(&post)
+
+	if err != nil {
+		render.JSON(w, r, err)
+		return
+	}
+
+	err = p.postService.UpdatePost(&post)
+
+	if err != nil {
+		render.JSON(w, r, err)
+		return
+	}
+
+	render.JSON(w, r, &post)
 }
 
 func (p *postUsecase) Delete(w http.ResponseWriter, r *http.Request) {
+	postID, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 64)
 
+	if err != nil {
+		render.JSON(w, r, err)
+		return
+	}
+
+	err = p.postService.DeletePost(postID)
+
+	if err != nil {
+		render.JSON(w, r, err)
+		return
+	}
+
+	render.JSON(w, r, "Post Successfully Deleted")
 }
