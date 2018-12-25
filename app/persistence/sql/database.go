50f5a0d3eb7818a49fba75527501115bbd158abf
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2018-12-25 14:36:11 +0800

    Added database persistence
    Signed-off-by: Richard Burk Orofeo <chardyy.orofeo@gmail.com>

--- app/persistence/sql/database.go
+++ app/persistence/sql/database.go
@@ -1,21 +1,47 @@
 package sql
 
 import (
-	"fmt"
 
 	// Import the mysql database
+	"fmt"
+	"log"
+
+	// Import mysql driver
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 )
 
-// New ...
-func New(dbName string) (*sqlx.DB, error) {
-	dsn := fmt.Sprintf("user=%s dbname=%s ssqlmode=%s", "root", dbName, "disable")
-	sqlDB, err := sqlx.Connect("mysql", dsn)
+var sqlDB *sqlx.DB
+var dbErr error
 
-	if err != nil {
-		return nil, err
+const dbName = "writeit"
+
+// New returns the pointer to sqlx.DB struct
+func New(dataSourceName string) (*sqlx.DB, error) {
+	sqlDB, dbErr = sqlx.Connect("mysql", dataSourceName)
+
+	if dbErr != nil {
+		return nil, dbErr
+	}
+	defer sqlDB.Close()
+
+	res := sqlDB.MustExec("CREATE DATABASE IF NOT EXISTS `" + dbName + "` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;")
+
+	if res == nil {
+		return nil, dbErr
+	}
+
+	log.Printf("CREATE RESULT: %v\n\n", res)
+
+	res = sqlDB.MustExec("USE " + dbName)
+
+	if res == nil {
+		return nil, dbErr
 	}
 
 	return sqlDB, nil
+}
+
+func createDatabase(dbName string) string {
+	return fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s;", dbName)
 }
