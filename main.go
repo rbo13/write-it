f9cd62b399a4cfcfe9848c26912250aef0310131
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2019-01-01 20:18:59 +0800

    Added middleware for validating a JWT token
    Signed-off-by: Richard Burk Orofeo <chardyy.orofeo@gmail.com>

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/go-chi/chi"
 	mw "github.com/go-chi/chi/middleware"
+	"github.com/go-chi/jwtauth"
 	"github.com/go-chi/render"
 
 	"github.com/rbo13/write-it/app/persistence/sql"
@@ -53,16 +56,23 @@ func main() {
 	router.Group(func(r chi.Router) {
 		// TODO :: Seek, verify and validate JWT tokens
 		// custom jwt verifier middleware
+		r.Use(jwtauth.Verifier(sql.TokenAuth))
 
 		// TODO :: Handle valid / invalid tokens. In this example, we use
 		// the provided authenticator middleware, but you can write your
 		// own very easily, look at the Authenticator method in jwtauth.go
 		// and tweak it, its not scary.
 		// custom jwt middleware authenticator
+		r.Use(jwtauth.Authenticator)
 
 		// API GROUP
 		r.Mount("/api/v1/users", routes.User(router, userUsecase))
 		r.Mount("/api/v1/posts", routes.Post(router, postUsecase))
+
+		r.Get("/dummy", func(w http.ResponseWriter, r *http.Request) {
+			_, claims, _ := jwtauth.FromContext(r.Context())
+			w.Write([]byte(fmt.Sprintf("protected area. hi %v", claims["email"])))
+		})
 	})
 
 	s := server.New(":1333", router)
