80905bc509d3c4675146421741112d3a22104b32
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2019-02-02 16:15:57 +0800

    Added tests to delete and get
    Signed-off-by: Richard Burk Orofeo <chardyy.orofeo@gmail.com>

--- app/persistence/cache/memcached/memcached_test.go
+++ app/persistence/cache/memcached/memcached_test.go
@@ -7,18 +7,47 @@ import (
 	"github.com/rbo13/write-it/app/persistence/cache/memcached"
 )
 
-var testKey = "myTestKey1"
+var (
+	testKey = "myTestKey1"
+)
 
-func TestSetCache(t *testing.T) {
+func TestMemcached(t *testing.T) {
 	mem := memcached.New("localhost", "11211", "localhost:11211")
 
-	ok, err := cache.Set(mem, testKey, "chardy")
+	t.Run("Set Cache", func(t *testing.T) {
+		ok, err := cache.Set(mem, testKey, "chardy")
 
-	if err != nil {
-		t.Error(err)
-	}
+		if err != nil {
+			t.Error(err)
+		}
 
-	if !ok {
-		t.Error("Error on saving")
-	}
+		if !ok {
+			t.Error("Error on saving")
+		}
+	})
+
+	t.Run("Get Cache", func(t *testing.T) {
+		val, err := cache.Get(mem, testKey)
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		t.Log(val)
+	})
+
+	t.Run("Delete Cache", func(t *testing.T) {
+		ok, err := cache.Delete(mem, testKey)
+
+		if err != nil {
+			t.Log(err)
+		}
+
+		if !ok {
+			t.Log("Error deleting value inside cache")
+		}
+
+		t.Log(ok)
+	})
+
 }
