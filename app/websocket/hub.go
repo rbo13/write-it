061c87e3436481b1cb9a6152274617037782d0ca
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2019-03-31 09:33:18 +0800

    Fixed hub.go and added TODO comment for proper implementation of websocket handlers
    Signed-off-by: Richard Burk Orofeo <chardyy.orofeo@gmail.com>

--- app/websocket/hub.go
+++ app/websocket/hub.go
@@ -24,9 +24,11 @@ func NewHub() *Hub {
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
 	}
+
 }
 
-func (hub *Hub) run() {
+// Run runs the websocket server
+func (hub *Hub) Run() {
 	for {
 		select {
 		case client := <-hub.register:
@@ -42,7 +44,8 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
-func (hub *Hub) handleWebSocket(w http.ResponseWriter, r *http.Request) {
+// HandleWebsocket handles websocket connection
+func (hub *Hub) HandleWebsocket(w http.ResponseWriter, r *http.Request) {
 	socket, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
@@ -71,14 +74,15 @@ func (hub *Hub) broadcast(message interface{}, ignore *Client) {
 
 func (hub *Hub) onConnect(client *Client) {
 	log.Println("client connected: ", client.socket.RemoteAddr())
+	// TODO:: implement properly onConnect
 	// Make list of all users
-	users := []message.User{}
-	for _, c := range hub.clients {
-		users = append(users, message.User{ID: c.id, Color: c.color})
-	}
+	// users := []message.User{}
+	// for _, c := range hub.clients {
+	// 	users = append(users, message.User{ID: c.id, Color: c.color})
+	// }
 	// Notify user joined
-	hub.send(message.NewConnected(client.color, users), client)
-	hub.broadcast(message.NewUserJoined(client.id, client.color), client)
+	// hub.send(message.NewConnected(client.color, users), client)
+	// hub.broadcast(message.NewUserJoined(client.id, client.color), client)
 }
 
 func (hub *Hub) onDisconnect(client *Client) {
@@ -97,24 +101,25 @@ func (hub *Hub) onDisconnect(client *Client) {
 	hub.clients[len(hub.clients)-1] = nil
 	hub.clients = hub.clients[:len(hub.clients)-1]
 	// Notify user left
-	hub.broadcast(message.NewUserLeft(client.id), nil)
+	// hub.broadcast(message.NewUserLeft(client.id), nil)
 }
 
 func (hub *Hub) onMessage(data []byte, client *Client) {
-	kind := gjson.GetBytes(data, "kind").Int()
-	if kind == message.KindStroke {
-		var msg message.Stroke
-		if json.Unmarshal(data, &msg) != nil {
-			return
-		}
-		msg.UserID = client.id
-		hub.broadcast(msg, client)
-	} else if kind == message.KindClear {
-		var msg message.Clear
-		if json.Unmarshal(data, &msg) != nil {
-			return
-		}
-		msg.UserID = client.id
-		hub.broadcast(msg, client)
-	}
+	log.Println("Implement onMessage")
+	// kind := gjson.GetBytes(data, "kind").Int()
+	// if kind == message.KindStroke {
+	// 	var msg message.Stroke
+	// 	if json.Unmarshal(data, &msg) != nil {
+	// 		return
+	// 	}
+	// 	msg.UserID = client.id
+	// 	hub.broadcast(msg, client)
+	// } else if kind == message.KindClear {
+	// 	var msg message.Clear
+	// 	if json.Unmarshal(data, &msg) != nil {
+	// 		return
+	// 	}
+	// 	msg.UserID = client.id
+	// 	hub.broadcast(msg, client)
+	// }
 }
