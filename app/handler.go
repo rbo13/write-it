96b74a715304b095321b57dfea7c64801ad97917
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2019-04-08 08:54:12 +0800

    Added handler to fetch all user posts
    Signed-off-by: Richard Burk Orofeo <chardyy.orofeo@gmail.com>

--- app/handler.go
+++ app/handler.go
@@ -1,21 +1,22 @@
 package app
 
 import (
-	"net/http"
+  "net/http"
 )
 
 // Handler is an interface that defines the basic operations of every http request.
 type Handler interface {
-	Create(w http.ResponseWriter, r *http.Request)
-	Get(w http.ResponseWriter, r *http.Request)
-	GetByID(w http.ResponseWriter, r *http.Request)
-	Update(w http.ResponseWriter, r *http.Request)
-	Delete(w http.ResponseWriter, r *http.Request)
+  Create(w http.ResponseWriter, r *http.Request)
+  Get(w http.ResponseWriter, r *http.Request)
+  GetByID(w http.ResponseWriter, r *http.Request)
+  Update(w http.ResponseWriter, r *http.Request)
+  Delete(w http.ResponseWriter, r *http.Request)
 }
 
 // UserHandler implements the Handler interface with some user related methods.
 type UserHandler interface {
-	Handler
+  Handler
 
-	Login(w http.ResponseWriter, r *http.Request)
+  Login(w http.ResponseWriter, r *http.Request)
+  GetUserPosts(w http.ResponseWriter, r *http.Request)
 }
